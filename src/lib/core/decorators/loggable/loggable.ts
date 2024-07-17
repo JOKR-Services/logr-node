f4baa217188a79ed25fe2bb99b2ea02627e964ee
Philip Guedes <philipguedes@gmail.com>
2024-07-17 19:19:09 -0300

    fix: loggable decorator instanceof

--- src/lib/core/decorators/loggable/loggable.ts
+++ src/lib/core/decorators/loggable/loggable.ts
@@ -1,16 +1,11 @@
-import { LoggerService } from '@core/interfaces';
-
-// TODO: change interface
-abstract class ZeusLogger {
-  constructor(protected logger: LoggerService) {}
-}
+import { Logr } from '@core/services';
 
 export function Loggable(loggerKey = 'logger') {
   return function (_: any, methodName: string, descriptor: PropertyDescriptor): PropertyDescriptor {
     const originalMethod = descriptor.value;
 
     descriptor.value = function (...args: any[]) {
-      if (this[loggerKey] instanceof ZeusLogger) {
+      if (this[loggerKey] instanceof Logr) {
         this[loggerKey].trigger = {
           methodName,
           className: this.constructor.name
