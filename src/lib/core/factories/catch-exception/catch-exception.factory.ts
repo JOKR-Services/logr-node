07058f31f2c8e9706e0cbd6a8bcd0460a6548cce
Iago Fagundes <iagof.elesbao@gmail.com>
2024-07-22 17:07:59 -0300

    fix: propagate the stack trace when using customErrorInstance (#47)
    * fix: propagate the stack trace when using customErrorInstance
    
    * chore: bump version to v2.7.1

--- src/lib/core/factories/catch-exception/catch-exception.factory.ts
+++ src/lib/core/factories/catch-exception/catch-exception.factory.ts
@@ -35,11 +35,18 @@ export function catchExceptionFactory(
         }
 
         if (!!options?.customErrorInstance) {
+          let newErr: any;
           if (typeof options?.customErrorInstance === 'function') {
-            throw options.customErrorInstance.call(this, e, this, ...args);
+            newErr = options.customErrorInstance.call(this, e, this, ...args);
+          } else {
+            newErr = options.customErrorInstance;
           }
 
-          throw options.customErrorInstance;
+          const stackTraceHead = newErr.stack.split('\n').slice(0, 2).join('\n');
+          const originalStackTrace = (e as any).stack;
+          newErr.stack = stackTraceHead + '\n' + originalStackTrace;
+
+          throw newErr;
         }
 
         if (options?.bubbleException || options?.typeErrorHandling === 'REGISTER') {
@@ -76,11 +83,18 @@ export function catchExceptionFactory(
         }
 
         if (!!options?.customErrorInstance) {
+          let newErr: any;
           if (typeof options?.customErrorInstance === 'function') {
-            throw options.customErrorInstance.call(this, e, this, ...args);
+            newErr = options.customErrorInstance.call(this, e, this, ...args);
+          } else {
+            newErr = options.customErrorInstance;
           }
 
-          throw options.customErrorInstance;
+          const stackTraceHead = newErr.stack.split('\n').slice(0, 2).join('\n');
+          const originalStackTrace = (e as any).stack;
+          newErr.stack = stackTraceHead + '\n' + originalStackTrace;
+
+          throw newErr;
         }
 
         if (options?.bubbleException || options?.typeErrorHandling === 'REGISTER') {
